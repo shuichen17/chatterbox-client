c0b634ed5fc64383df8d66c09cbf2c6fcbe790eb
shuichen17 <shuichen17@gmail.com>
2019-04-19 16:20:44 -0700

    Implement 1 message onto the DOM

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -20,7 +20,8 @@ var App = {
   fetch: function(callback = ()=>{}) {
     Parse.readAll((data) => {
       // examine the response from the server request:
-      console.log(data);
+      MessagesView.render(data)
+      // console.log(data);
 
       callback();
     });
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -5,7 +5,29 @@ var MessagesView = {
   initialize: function() {
   },
 
-  render: function() {
+  render: function(data) {
+    console.log(data)
+
+  //  for (var i = 0; i < 100; i++) {
+      
+    var message = data.results[0].text
+    var user = data.results[0].username
+    console.log(message, 'this is the message');
+    console.log(user, 'this is the user')
+ 
+ 
+ 
+    var temp = MessageView.render(); 
+    this.$chats.append(temp);
+    $('.username').html(user)
+    $('.message').html(message)
+
+
+  //  }
+    
+
+
   }
+
 
 };
